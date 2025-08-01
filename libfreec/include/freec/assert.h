60330ac88c7ba67418675efa9c357024f3b06160
dlarudgus20 <dlarudgus20@naver.com>
2025-08-01 17:29:16 +0900

    Launch & Attach Qemu panic() fix

--- libfreec/include/freec/assert.h
+++ libfreec/include/freec/assert.h
@@ -7,7 +7,8 @@
 #undef assert
 #endif
 
-#define panic(msg) (panic_impl(msg, "", __FILE__, __func__, __LINE__))
+#define panic(msg) (panic_impl((#msg)[0] ? "panic : " msg : "panic", __FILE__, __func__, __LINE__))
+
 #define ASSERT_1(exp, ...) ((void)((exp) || (panic_impl("assertion failed : " #exp, __FILE__, __func__, __LINE__), 1)))
 #define ASSERT_2(exp, msg) ((void)((exp) || (panic_impl(msg " : " #exp, __FILE__, __func__, __LINE__), 1)))
 
