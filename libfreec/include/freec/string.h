57f0ab275e4912e25387d607fd4c67ee094baeb8
dlarudgus20 <dlarudgus20@naver.com>
2025-08-13 21:45:57 +0900

    memmove typo

--- libfreec/include/freec/string.h
+++ libfreec/include/freec/string.h
@@ -5,7 +5,7 @@
 #include <stddef.h>
 
 void* memcpy(void* restrict dest, const void* restrict src, size_t count);
-void* memmmove(void* dest, const void* src, size_t count);
+void* memmove(void* dest, const void* src, size_t count);
 void* memset(void* dest, int ch, size_t count);
 int memcmp(const void* lhs, const void* rhs, size_t count);
 void* memchr(const void* ptr, int ch, size_t count);
--- libfreec/src/string.c
+++ libfreec/src/string.c
@@ -11,7 +11,7 @@ void* memcpy(void* restrict dest, const void* restrict src, size_t count) {
     return dest;
 }
 
-void* memmmove(void* dest, const void* src, size_t count) {
+void* memmove(void* dest, const void* src, size_t count) {
     const char* s = src;
     char* d = dest;
     if (d <= s) {
